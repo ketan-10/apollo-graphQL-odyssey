a1c76ba2c687c813b119d2b263560c3bb80cc1d9
wiwium <ketanchaudhari1998@gmail.com>
2021-07-07 00:13:44 +0530

    added ApolloClient for react

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
 
+import {ApolloClient,ApolloProvider, InMemoryCache} from "@apollo/client"
+
+const client = new ApolloClient({
+  uri: 'https://localhost:4000',
+  cache: new InMemoryCache(),
+});
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
