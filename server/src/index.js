84bd2724a3cc471a0ddd07c70c820dd97080c1fa
wiwium <ketanchaudhari1998@gmail.com>
2021-07-06 22:25:29 +0530

    added ApolloServer with mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,13 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+const server = new ApolloServer({typeDefs, mocks:true});
+
+server.listen().then((serverInfo)=>{
+  console.log(`
+    🚀 Server is running!
+    🔉 Listening on port ${serverInfo.port}
+    🔗 Server Link ${serverInfo.url}
+    📭 Query at https://studio.apollographql.com/dev
+  `);
+});
